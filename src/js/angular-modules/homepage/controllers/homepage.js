bc0cd4c3c293a13605c62a28481790596e18facc
Christopher Weir <christopher_weir@me.com>
2014-12-08 22:47:14 +0000

    Tests

--- src/js/angular-modules/homepage/controllers/homepage.js
+++ src/js/angular-modules/homepage/controllers/homepage.js
@@ -5,10 +5,11 @@ angular.module('home')
         '$scope',
         '$sce',
         '$ilnPosts',
-        function ( $scope, $sce, $ilnPosts ) {
+        '$ilnTaxonomies',
+        function ( $scope, $sce, $ilnPosts, $ilnTaxonomies ) {
 
             $scope.posts = null;
-            
+
             $scope.to_trusted = function(html_code) {
                 return $sce.trustAsHtml(html_code);
             }
@@ -16,7 +17,11 @@ angular.module('home')
             $ilnPosts.getPosts(function( _data ){
                 $scope.posts = _data;
             });
-            
+
+            $ilnTaxonomies.getCategories(function( _data ){
+                console.log( _data );
+            });
+
         }
     ]);
 
